9ab144c956a5149f395e5359dbfbd914e875aeb2
Vincent Chov <vincentchov@gmail.com>
2020-09-27 15:07:36 -0400

    chore(package.json,webpack-config): add webpack-dev-server with sourcemaps and some npm scripts

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,25 +1,35 @@
 import * as AppRootPath from "app-root-path";
+import WebpackUserscript from "webpack-userscript";
 
 import {
-    createWebpackConfig,
-    DEFAULT_BUILD_CONFIG,
-    DEFAULT_METADATA_SCHEMA,
+  createWebpackConfig,
+  DEFAULT_BUILD_CONFIG,
+  DEFAULT_METADATA_SCHEMA,
 } from "userscripter/build";
 import METADATA from "./metadata";
 import * as CONFIG from "./src/config";
 import * as SITE from "./src/site";
 import U from "./src/userscript";
 
-export default createWebpackConfig({
-    buildConfig: {
-        ...DEFAULT_BUILD_CONFIG({
-            rootDir: AppRootPath.path,
-            id: U.id,
-            now: new Date(),
-        }),
-        sassVariables: { CONFIG, SITE },
-    },
-    metadata: METADATA,
-    metadataSchema: DEFAULT_METADATA_SCHEMA,
-    env: process.env,
+const webpackConfig = createWebpackConfig({
+  buildConfig: {
+    ...DEFAULT_BUILD_CONFIG({
+      rootDir: AppRootPath.path,
+      id: U.id,
+      now: new Date(),
+    }),
+    sassVariables: { CONFIG, SITE },
+  },
+  metadata: METADATA,
+  metadataSchema: DEFAULT_METADATA_SCHEMA,
+  env: process.env,
 });
+
+export default {
+  ...webpackConfig,
+  devServer: {
+    contentBase: `${AppRootPath.path}/dist`,
+  },
+  devtool: "eval-source-map",
+  plugins: [new WebpackUserscript()],
+};
